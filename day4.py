96e7852cc5390ec893126e8cf75828573b3b6711
manvikkhandal <manvikkhandal10s@gmail.com>
2024-07-20 11:06:51 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,38 @@
+#marks = {'mohit':52,'rohit':56,'rocky':53,'mohit':54}
+# print(marks)
+# print(type(marks))
+#print(marks['rakesh'])
+#print(marks['mohit'])
+
+# marks['rohit'] = 66 #change 
+# print(marks)
+# marks['rahul']= 100 #insert
+# print(marks)
+# print(marks.keys())
+# print(marks.values())
+
+# marks.pop('mohit')
+# print(marks)
+
+# students = {'name':['rahul','mohit','ritu','yash','jugnu'],'Marks':[52,14,36,45,96],'subject':"science"}
+# #single variable = 5 students, name , marks, subject
+# print(students)
+# print(students['Marks'])
+# print(students['name'])
+# students['subject']= "physics"
+# print(students['subject'])
+
+# students['name'].remove('ritu')
+# students['Marks'].pop(2)
+
+# students['Marks']=41
+# print(students)
+
+
+##operators
+
+num1 = input('plz enter first number:')
+num2 = input('plz enter second number:')
+
+output = num1 + num2
+print(output)
