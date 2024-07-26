b8d2511215f484351f29f5e053398ea1003cc828
manvikkhandal <manvikkhandal10s@gmail.com>
2024-07-26 21:52:01 +0530

    Add files via upload

---
+++ portfolio/app.py
@@ -0,0 +1,52 @@
+from flask import Flask,render_template,url_for,request
+import sqlite3 
+
+
+app = Flask(__name__)
+
+@app.route('/')  # http://127.0.0.1:5000
+def home():
+    return render_template('home.html')
+
+@app.route('/about')  # http://127.0.0.1:5000/about 
+def about():
+    return render_template('about.html')
+
+@app.route('/service')  # http://127.0.0.1:5000/service
+def service():
+    return render_template('service.html')
+
+@app.route('/queryus')   #http://127.0.0.1:5000/queryus
+def queryus():
+    return render_template('queryus.html')
+
+
+@app.route('/user_query',methods=['GET','POST'])
+def user_query():
+    if request.method == "POST":  
+        conn =  sqlite3.connect("userdata.db")
+        name = request.form['name']
+        age = int(request.form['age'])
+        address = request.form['address']
+        college = request.form['college']
+        branch = request.form['branch']
+        roll_no = int(request.form['roll_no'])
+        query_sub = request.form['query_sub']
+
+        user_data = (name,age,address,college,branch,roll_no,query_sub)
+        ## database 
+        insert_data_query = """
+        insert into userecord values(?,?,?,?,?,?,?)
+        """
+        cur = conn.cursor()
+        cur.execute(insert_data_query,user_data)
+        print("You have successfully inserted your data into table : ",user_data)
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        return  "Your data is Submitted into the data base!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",debug=True)
